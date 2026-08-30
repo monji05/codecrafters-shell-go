bc9a65f69f36cd1ffd6cf3fe02be512d595863e1
monji05 <monji4869@gmail.com>
2026-08-30 15:45:28 +0900

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -44,9 +44,9 @@ func main() {
 			}
 		} else {
 			fmt.Printf("Program was passed %d args (including program name).\n", len(tokens))
-			fmt.Printf("Arg #0 (program name): %s", cmd)
+			fmt.Printf("Arg #0 (program name): %s\n", cmd)
 			for index, arg := range args {
-				fmt.Printf("Arg #%d: %s", index+1, arg)
+				fmt.Printf("Arg #%d: %s\n", index+1, arg)
 			}
 		}
 	}
