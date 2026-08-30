789b4dd7208b6382ecd1f449b8c1916fcf1bdf4d
monji05 <monji4869@gmail.com>
2026-08-30 15:41:49 +0900

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -43,8 +43,15 @@ func main() {
 				fmt.Printf("%s: not found \n", args[0])
 			}
 		} else {
-			fmt.Printf("%s: command not found \n", cmd)
+			if _, err := exec.LookPath(args[0]); err == nil {
+				fmt.Printf("Program was passed %d args \n", len(args))
+				fmt.Printf("Arg #0 (program name): %s", cmd)
+				for index, arg := range args {
+					fmt.Printf("Arg #%d: %s", index+1, arg)
+				}
+			} else {
+				fmt.Printf("%s: command not found \n", cmd)
+			}
 		}
-
 	}
 }
